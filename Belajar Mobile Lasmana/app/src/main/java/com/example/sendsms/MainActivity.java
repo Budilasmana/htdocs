d0028d323726f8c5a0457ce4ca9889da24554e71
Budilasmana <budylasmana07@gmail.com>
2019-03-12 11:29:59 -0700

    ci dan bootstrap

---
+++ Belajar Mobile Lasmana/app/src/main/java/com/example/sendsms/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.sendsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendBtn = (Button) findViewById(R.id.btn_kirim);
+        txtphoneNo = (EditText) findViewById(R.id.ed_no);
+        txtMessage = (EditText) findViewById(R.id.ed_pesan);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage();
+            }
+        });
+    }
+
+    protected void sendSMSMessage() {
+        Log.i("Send SMS", "");
+        String phoneNo = txtphoneNo.getText().toString();
+        String message = txtphoneNo.getText().toString();
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS send.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                
+    }
+    }
+}
