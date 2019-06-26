eda715cd527cc7cb2c1ce70c227eac3a869abb15
Fahriza Ramadhani Putra <fahrizarp01@gmail.com>
2019-06-26 22:20:26 +0700

    Update sidebar.php

--- projectsemester4/application/views/admin/_partials/sidebar.php
+++ projectsemester4/application/views/admin/_partials/sidebar.php
@@ -1,33 +1,18 @@
 <ul class="sidebar navbar-nav">
-  <!-- <li class="nav-item active">
-          <a class="nav-link" href="<?= site_url("profil") ?>">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Profil</span>
-          </a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="<?= site_url("charts") ?>">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Kontak</span></a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="<?= site_url("tables") ?>">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Artikel</span></a>
-        </li> -->
+
   <li class="nav-item active">
     <a class="nav-link" href="<?php echo site_url('admin/pengguna/index') ?>">
-      <i class="fas fa-fw fa-table"></i>
+      <i class=" fas fa-users-cog"></i>
       <span>Daftar Pelanggan</span></a>
   </li>
   <li class="nav-item active">
     <a class="nav-link" href="<?php echo site_url('admin/produk/index') ?>">
-      <i class="fas fa-fw fa-table"></i>
+      <i class="fas fa-chart-pie"></i>
       <span>Produk</span></a>
   </li>
   <li class="nav-item active">
     <a class="nav-link" href="<?php echo site_url('admin/transaksi/index') ?>">
-      <i class="fas fa-fw fa-table"></i>
+      <i class="fas fa-chart-bar"></i>
       <span>Riwayat Transaksi</span></a>
   </li>
 </ul>
